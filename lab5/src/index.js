5106dc3daa2d751571fc99af09a2c204928bb603
Wiktor Płużek <w.pluzek.409@studms.ug.edu.pl>
2021-03-24 11:56:59 +0100

    lab5-index.js

---
+++ lab5/src/index.js
@@ -0,0 +1,17 @@
+const status = 'Completed'
+
+switch(status) {
+    case 'Completed':
+        runFunc();
+        break
+    case 'Running': 
+        runFunc2();
+}
+
+function runFunc(){
+
+}
+
+function runFunc2(){
+
+}
